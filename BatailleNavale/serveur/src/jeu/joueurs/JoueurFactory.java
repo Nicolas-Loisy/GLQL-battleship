e289052364d515b6d9dbf87bf77ee62a7eed27e4
Nicolas Loisy <nicolas.loisy@yahoo.fr>
2023-05-24 02:56:25 +0200

    Modif logger

--- BatailleNavale/serveur/src/jeu/joueurs/JoueurFactory.java
+++ BatailleNavale/serveur/src/jeu/joueurs/JoueurFactory.java
@@ -3,9 +3,14 @@ package jeu.joueurs;
 import java.io.IOException;
 import java.net.Socket;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import jeu.reseau.Serveur;
 
 public class JoueurFactory {
+
+  protected static final Logger logger = LogManager.getLogger(JoueurFactory.class);
   private static Serveur serveur;
 
   public static void setServeur(Serveur serveur) {
@@ -13,7 +18,7 @@ public class JoueurFactory {
   }
   
   public static iJoueur creerJoueur(TypeJoueur JoueurType) throws IOException {
-    System.out.println("[SERVEUR] Création d'un joueur de type " + JoueurType);
+    logger.info("[SERVEUR] Création d'un joueur de type " + JoueurType);
     
     switch(JoueurType) {
     
